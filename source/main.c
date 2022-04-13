78be2a6f125bbf53c20dbb25e7fc181b06f932e1
Kyle Coffey <me@phytolizer.dev>
2022-04-13 05:41:12 -0500

    use CurrentUser in the main program

--- source/main.c
+++ source/main.c
@@ -1,13 +1,23 @@
 #include "monkey.h"
 #include "monkey/repl.h"
+#include "monkey/user.h"
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char* argv[]) {
 	(void)argc;
 	(void)argv;
 
+	char* user = CurrentUser();
+	if (user == NULL) {
+		fprintf(stderr, "Could not get current user.\n");
+		return EXIT_FAILURE;
+	}
+	printf("Hello %s! This is the Monkey programming language!\n", user);
+	free(user);
+	printf("Feel free to type in commands\n");
 	MONKEY_REPL(.reader = StreamFromFile(stdin), .writer = StreamFromFile(stdout));
-	return 0;
+	return EXIT_SUCCESS;
 }
