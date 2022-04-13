bf0080ce9dd13646f1c1cdebe3d5fb9d674dfbc1
Kyle Coffey <me@phytolizer.dev>
2022-04-13 06:32:38 -0500

    fix formatting

--- source/span.h
+++ source/span.h
@@ -3,14 +3,14 @@
 #include <stddef.h>
 
 #define SPAN_TYPE(T) \
-    struct { \
-        T* begin; \
-        T* end; \
-        size_t length; \
-    }
+	struct { \
+		T* begin; \
+		T* end; \
+		size_t length; \
+	}
 
 #define SPAN_FROM_BOUNDS(begin, end) \
-    { (begin), (end), (end) - (begin) }
+	{ (begin), (end), (end) - (begin) }
 
 #define SPAN_WITH_LENGTH(begin, length) \
-    { (begin), (begin) + (length), (length) }
+	{ (begin), (begin) + (length), (length) }
