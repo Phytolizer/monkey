c51ba13cb03c99b82f7153af0f174247e6611457
Kyle Coffey <me@phytolizer.dev>
2022-04-13 05:32:36 -0500

    cross-platform user impl (not tested on OSX)

---
+++ source/monkey/user.c
@@ -0,0 +1,37 @@
+#include "monkey/user.h"
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include "monkey/string.h"
+
+#include <pwd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+char* CurrentUser(void) {
+#ifdef _WIN32
+	ULONG size = 0;
+	GetUserNameEx(NameDisplay, NULL, &size);
+	char* result = malloc(size);
+	GetUserNameEx(NameDisplay, result, &size);
+	return result;
+#else
+#define DEFAULT_MAX_NAME_LENGTH 16384
+	uid_t uid = getuid();
+	long bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+	if (bufsize == -1) {
+		bufsize = DEFAULT_MAX_NAME_LENGTH;
+	}
+	char* buf = malloc((size_t)bufsize);
+	struct passwd pw;
+	struct passwd* result;
+	getpwuid_r(uid, &pw, buf, (size_t)bufsize, &result);
+	if (result == NULL) {
+		free(buf);
+		return NULL;
+	}
+	return MonkeyStrdup(pw.pw_name);
+#endif
+}
---
+++ test/source/user_test.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch.hpp>
+#include <memory>
+
+extern "C" {
+#include "monkey/user.h"
+}
+
+TEST_CASE("CurrentUser() returns a valid name", "[user]") {
+	char* name = CurrentUser();
+	auto name_ptr = std::unique_ptr<char, decltype(&free)>(name, &free);
+	REQUIRE(name != nullptr);
+}
